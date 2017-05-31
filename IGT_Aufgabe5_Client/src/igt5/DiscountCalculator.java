8bc3f6bad6abbb6c150da871a9c6d46837477730
Philipp Minges <fjnalta@gmail.com>
2017-06-01 00:06:14 +0200

    2.0

---
+++ IGT_Aufgabe5_Client/src/igt5/DiscountCalculator.java
@@ -0,0 +1,34 @@
+package igt5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Client Program for DiscountCalculator WSDL
+ * @author ajo
+ */
+public class DiscountCalculator {
+
+	public static void main(String[] args) {
+		
+		while(true) {
+			try {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				System.out.print("Please insert the Username: ");
+				String username = br.readLine();
+				
+				DiscountCalculatorStub stub = new DiscountCalculatorStub();
+				DiscountCalculatorStub.GetDiscount myDiscount = new DiscountCalculatorStub.GetDiscount();
+				
+				myDiscount.setUsername(username);
+				DiscountCalculatorStub.GetDiscountResponse myResponse = stub.getDiscount(myDiscount);
+				
+				System.out.println(myResponse);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
